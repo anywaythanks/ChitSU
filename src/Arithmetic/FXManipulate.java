dfd450940d8f4bf67808c2cbdeec35326a237258
anywaythanks <anywaythanks@mail.ru>
2020-07-19 18:53:01 +0300

    Добавлена документация.

--- src/Arithmetic/FXManipulate.java
+++ src/Arithmetic/FXManipulate.java
@@ -2,8 +2,18 @@ package Arithmetic;
 
 import javafx.scene.Node;
 import javafx.stage.Stage;
-
+/**
+ * Класс для манипуляций с JavaFX, в частности с {@link Node}.
+ *
+ * @author anywaythanks
+ * @version 1.0
+ */
 public class FXManipulate {
+    /**
+     * Получить {@link Stage} из любого {@link Node}.
+     * @param node {@link Node}, из которого нужно получить {@link Stage}.
+     * @return {@link Stage}.
+     */
     public static Stage getStage(Node node) {
         return (Stage) node.getScene().getWindow();
     }
