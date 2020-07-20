4803b4e213ac09797ef4ab039c7c61e3fb28f6fb
anywaythanks <anywaythanks@mail.ru>
2020-07-20 19:03:06 +0300

    Новые функции: getRGBAModel(Color color); setOpacity(Color color, double newOpacity);

--- src/Arithmetic/FXManipulate.java
+++ src/Arithmetic/FXManipulate.java
@@ -1,7 +1,9 @@
 package Arithmetic;
 
 import javafx.scene.Node;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
 /**
  * Класс для манипуляций с JavaFX, в частности с {@link Node}.
  *
@@ -11,11 +13,34 @@ import javafx.stage.Stage;
 public class FXManipulate {
     /**
      * Получить {@link Stage} из любого {@link Node}.
+     *
      * @param node {@link Node}, из которого нужно получить {@link Stage}.
      * @return {@link Stage}.
      */
     public static Stage getStage(Node node) {
         return (Stage) node.getScene().getWindow();
+    }
+
+    /**
+     * Получить RedGreenBlueAlpha модель {@link Color}.
+     *
+     * @param color {@link Color}, модель которого нужно изменить.
+     * @return RedGreenBlueAlpha модель в виде {@link String}.
+     */
+    public static String getRGBAModel(Color color) {
+        return "rgba(" + (int) (color.getRed() * 255) + "," + (int) (color.getGreen() * 255) + "," + (int) (color.getBlue() * 255) + "," + color.getOpacity() + ")";
+    }
+
+
+    /**
+     * Задать прозрачность {@link Color}
+     *
+     * @param color      {@link Color}, прозрачность которого нужно изменить
+     * @param newOpacity новое значение прозрачности для цвета
+     * @return {@link Color} с новой прозрачностью
+     */
+    public static Color setOpacity(Color color, double newOpacity) {
+        return Color.valueOf("rgba(" + (int) (color.getRed() * 255) + "," + (int) (color.getGreen() * 255) + "," + (int) (color.getBlue() * 255) + "," + newOpacity + ")");
     }
 
 }
