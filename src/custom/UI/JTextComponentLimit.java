26a5334d3d1c748fee1178a4994dece847caa083
anywaythanks <anywaythanks@mail.ru>
2021-03-07 14:49:35 +0300

    Создание класса для лимитирования JTextComponent

---
+++ src/custom/UI/JTextComponentLimit.java
@@ -0,0 +1,22 @@
+package custom.UI;
+
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.PlainDocument;
+
+public class JTextComponentLimit extends PlainDocument {
+    private final int limit;
+
+    public JTextComponentLimit(int limit) {
+        super();
+        this.limit = limit;
+    }
+
+    public void insertString(int offset, String str, AttributeSet attr) throws BadLocationException {
+        if (str == null) return;
+
+        if ((getLength() + str.length()) <= limit) {
+            super.insertString(offset, str, attr);
+        }
+    }
+}
